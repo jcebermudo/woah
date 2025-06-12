78af5dd6413ef42474e8a38ae8b3b0fe0ec1bf1e
James <jamestech718@gmail.com>
2025-06-12 16:15:28 +0800

    Enhance animation effects in Home component by wrapping image elements with motion.div for smoother transitions

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,41 @@ import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <div>
-      <div className="select-none flex flex-row gap-[10px] items-center justify-center">
-        <div className="ml-[10px]">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div
+        className="select-none flex flex-row gap-[10px] items-center justify-center"
+
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 0.2, type: "spring", stiffness: 200 }}
+          className="ml-[10px]"
+        >
           <Image src="/png/w.png" alt="W" width={62.94} height={53.12} />
-        </div>
-        <div className="ml-[-15px]">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 200, delay: 0.2 }} className="ml-[-15px]">
           <Image src="/png/o.png" alt="W" width={45.08} height={53.58} />
-        </div>
-        <div className="ml-[-15px]">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 200, delay: 0.4 }}
+          className="ml-[-15px]"
+        >
           <Image src="/png/a.png" alt="W" width={47.73} height={52.11} />
-        </div>
-        <div className="ml-[-12px]">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 200, delay: 0.6 }}
+          className="ml-[-12px]"
+        >
           <Image src="/png/h.png" alt="W" width={47.87} height={52.93} />
-        </div>
+        </motion.div>
       </div>
 
       <h1>Hello World</h1>

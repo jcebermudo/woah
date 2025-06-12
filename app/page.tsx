82285ebaf80f202cc2888aac871ab19c3a1473a7
James <jamestech718@gmail.com>
2025-06-12 16:10:30 +0800

    Refactor image layout in Home component for improved alignment and spacing

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,20 @@ export default function Home() {
   return (
     <div>
       <div className="select-none flex flex-row gap-[10px] items-center justify-center">
-        <Image src="/png/w.png" alt="W" width={62.94} height={53.12} />
-        <Image src="/png/o.png" alt="W" width={45.08} height={53.58} />
-        <Image src="/png/a.png" alt="W" width={47.73} height={52.11} />
-        <Image src="/png/h.png" alt="W" width={47.87} height={52.93} />
+        <div className="ml-[10px]">
+          <Image src="/png/w.png" alt="W" width={62.94} height={53.12} />
+        </div>
+        <div className="ml-[-15px]">
+          <Image src="/png/o.png" alt="W" width={45.08} height={53.58} />
+        </div>
+        <div className="ml-[-15px]">
+          <Image src="/png/a.png" alt="W" width={47.73} height={52.11} />
+        </div>
+        <div className="ml-[-12px]">
+          <Image src="/png/h.png" alt="W" width={47.87} height={52.93} />
+        </div>
       </div>
+
       <h1>Hello World</h1>
     </div>
   );

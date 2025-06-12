ed46ae45f21121be1974d328c1415224d6829efe
James <jamestech718@gmail.com>
2025-06-12 18:56:37 +0800

    Add validateUsername function to check for available usernames in the database

---
+++ actions/validateUsername.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { eq } from 'drizzle-orm';
+import { usersTable } from '../src/db/schema';
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+export async function validateUsername(username: string) {
+
+  const takenUsernames = await db.select().from(usersTable).where(eq(usersTable.userName, username.toLowerCase()))
+
+  // Simulate network delay
+  await new Promise(resolve => setTimeout(resolve, 500))
+
+  return takenUsernames.length === 0
+}

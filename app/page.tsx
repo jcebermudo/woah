4107a8051db343ce305a5753df9b1f68b536254b
James <jamestech718@gmail.com>
2025-06-12 17:24:25 +0800

    Update initial animation scale in Home component for smoother transitions

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="select-none flex flex-row gap-[10px] items-center justify-center">
         <motion.div
-          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ duration: 0.2, type: "spring", stiffness: 200 }}
           className="ml-[10px]"
@@ -16,7 +16,7 @@ export default function Home() {
           <Image src="/png/w.png" alt="W" width={79.18} height={66.83} />
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{
             duration: 0.5,
@@ -29,7 +29,7 @@ export default function Home() {
           <Image src="/png/o.png" alt="W" width={56.71} height={67.41} />
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{
             duration: 0.5,
@@ -42,7 +42,7 @@ export default function Home() {
           <Image src="/png/a.png" alt="W" width={60.04} height={65.56} />
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, scale: 0.7, y: 70 }}
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{
             duration: 0.5,

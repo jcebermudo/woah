5ecfc504c0549d29e9130a3e729ea9a2b21e736d
James <jamestech718@gmail.com>
2025-06-13 16:25:20 +0800

    Add AuthPage component with animations and messaging; recreate db schema and index files

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,74 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "motion/react";
+
 export default function AuthPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <div className="select-none flex flex-row gap-[10px] items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{ duration: 0.2, type: "spring", stiffness: 200 }}
+          className="ml-[10px]"
+        >
+          <Image src="/png/w.png" alt="W" width={79.18} height={66.83} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 200,
+            delay: 0.1,
+          }}
+          className="ml-[-15px]"
+        >
+          <Image src="/png/o.png" alt="W" width={56.71} height={67.41} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 200,
+            delay: 0.2,
+          }}
+          className="ml-[-15px]"
+        >
+          <Image src="/png/a.png" alt="W" width={60.04} height={65.56} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, y: 70 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 200,
+            delay: 0.3,
+          }}
+          className="ml-[-12px]"
+        >
+          <Image src="/png/h.png" alt="W" width={60.22} height={66.59} />
+        </motion.div>
+      </div>
+
+      <motion.p
+        initial={{ opacity: 0, y: 70 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.5,
+          type: "spring",
+          stiffness: 200,
+          delay: 0.4,
+        }}
+        className="text-[24px] font-medium text-foreground"
+      >
+        Send anonymous messages
+      </motion.p>
       
     </div>
   );

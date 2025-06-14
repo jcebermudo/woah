6ad0b15d49fb7b6cd753e4d94e113af80ff38626
James <jamestech718@gmail.com>
2025-06-14 22:46:38 +0800

    Refactor main page structure to enhance layout and integrate Toolbox and SettingsPanel components for improved user interaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+// pages/index.js
 
-export default function Home() {
+import React from "react";
+
+import { Toolbox } from "./components/Toolbox";
+import { SettingsPanel } from "./components/SettingsPanel";
+import { Topbar } from "./components/Topbar";
+
+import { Container } from "./components/user/Container";
+import { Button } from "./components/user/Button";
+import { Card } from "./components/user/Card";
+import { Text } from "./components/user/Text";
+
+export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Hello World</h1>
+    <div style={{ margin: "0 auto", width: "800px" }}>
+      <h1>A super simple page editor</h1>
+      <Topbar />
+      <Container padding={5} background="#eee">
+        <Card background="#fff" padding={5} />
+      </Container>
+      <div style={{ paddingTop: "10px" }}>
+        <div>
+          <Container padding={5} background="#eee">
+            <Card background="#fff" padding={5}  />
+          </Container>
+        </div>
+        <div>
+          <Toolbox />
+          <SettingsPanel />
+        </div>
+      </div>
     </div>
   );
 }

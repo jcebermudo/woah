702363fd65d018cd7ab1fa2b6147baeb1b380b51
James <jamestech718@gmail.com>
2025-06-15 03:22:58 +0800

    Implement drag-and-drop functionality in User Container component using Craft.js for enhanced interactivity.

--- app/components/user/container.jsx
+++ app/components/user/container.jsx
@@ -1,9 +1,11 @@
 // components/user/Container.js
 import React from "react";
-
+import { useNode } from "@craftjs/core";
+  
 export const Container = ({background, padding = 0, children}) => {
+  const { connectors: {connect, drag} } = useNode();
   return (
-    <div style={{margin: "5px 0", background, padding: `${padding}px`}}>
+    <div ref={ref => connect(drag(ref))} style={{margin: "5px 0", background, padding: `${padding}px`}}>
       {children}
     </div>
   )

2e29c2440a5e9c5717451b4c591c72270c94f13c
James <jamestech718@gmail.com>
2025-06-15 03:23:12 +0800

    Refactor App component to integrate Craft.js Editor and enhance drag-and-drop functionality for layout management.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import { Toolbox } from "./components/Toolbox";
@@ -9,26 +11,46 @@ import { Button } from "./components/user/Button";
 import { Card } from "./components/user/Card";
 import { Text } from "./components/user/Text";
 
+import { Editor, Frame, Element } from "@craftjs/core";
+
 export default function App() {
   return (
     <div className="mx-auto w-[800px]">
       <h1 className="text-xl font-semibold text-center">
         A super simple page editor
       </h1>
-      <div className="flex gap-3 pt-3">
-        <Topbar />
-        <div className="flex-1">
-          <Container padding={5} background="#eee">
-            <Card background="#fff" padding={5} />
-          </Container>
-        </div>
-        <div className="w-1/4">
-          <div className="bg-card rounded-lg shadow-sm">
-            <Toolbox />
-            <SettingsPanel />
+      <Editor resolver={{ Card, Button, Text, Container }}>
+        <div className="flex gap-3">
+          <div className="flex-1">
+            <Frame>
+              <Element is={Container} padding={5} background="#eee" canvas>
+                <Container padding={5} background="#eee">
+                  <Element is={Container} padding={5} background="#eee" canvas>
+                    <Card background="#fff" padding={5} />
+                  </Element>
+                  <Element is={Container} padding={5} background="#eee" canvas>
+                    <Button size="sm" variant="outline" color="default">
+                      Click
+                    </Button>
+                  </Element>
+                  <Element is={Container} padding={5} background="#eee" canvas>
+                    <Text text="Hi world!" fontSize="14px" />
+                  </Element>
+                  <Element is={Container} padding={6} background="#999" canvas>
+                    <Text text="It's me again!" fontSize="14px" />
+                  </Element>
+                </Container>
+              </Element>
+            </Frame>
+          </div>
+          <div className="w-1/4">
+            <div className="bg-card rounded-lg shadow-sm">
+              <Toolbox />
+              <SettingsPanel />
+            </div>
           </div>
         </div>
-      </div>
+      </Editor>
     </div>
   );
 }

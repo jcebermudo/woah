5530f9b99258b2f6d9df8be59f740fd07c0c60bc
James <jamestech718@gmail.com>
2025-06-15 03:23:03 +0800

    Integrate drag-and-drop functionality into Card component using Craft.js for enhanced interactivity.

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -3,10 +3,12 @@ import React  from "react";
 import { Text } from "./Text";
 import { Button } from "./Button";
 import { Container } from "./Container";
+import { useNode } from "@craftjs/core";
 
 export const Card = ({background, padding = 20}) => {
+  const { connectors: {connect, drag} } = useNode();
   return (
-    <Container background={background} padding={padding}>
+    <Container ref={ref => connect(drag(ref))} background={background} padding={padding}>
       <div className="text-only">
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />

3349cde3fb37540256318e9dcca3fee797f71b90
James <jamestech718@gmail.com>
2025-06-14 23:39:16 +0800

    Refactor User Container component to replace Material-UI Paper with a div for improved consistency and alignment with custom UI standards.

--- app/components/user/container.jsx
+++ app/components/user/container.jsx
@@ -1,11 +1,10 @@
 // components/user/Container.js
 import React from "react";
-import { Paper } from "@mui/material";
 
 export const Container = ({background, padding = 0, children}) => {
   return (
-    <Paper style={{margin: "5px 0", background, padding: `${padding}px`}}>
+    <div style={{margin: "5px 0", background, padding: `${padding}px`}}>
       {children}
-    </Paper>
+    </div>
   )
 }

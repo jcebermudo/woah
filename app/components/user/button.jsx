26c1328ca1b6592d2387163cd5637e52e29ca135
James <jamestech718@gmail.com>
2025-06-15 03:23:07 +0800

    Integrate drag-and-drop functionality into Button component using Craft.js for enhanced interactivity.

--- app/components/user/button.jsx
+++ app/components/user/button.jsx
@@ -1,10 +1,12 @@
 // components/user/Button.js
 import React  from "react";
 import { Button as ShadButton } from "@/components/ui/button";
+import { useNode } from "@craftjs/core";
 
 export const Button = ({size, variant, color, children}) => {
+  const { connectors: {connect, drag} } = useNode();
   return (
-    <ShadButton size={size} variant={variant} color={color}>
+    <ShadButton ref={ref => connect(drag(ref))} size={size} variant={variant} color={color}>
       {children}
     </ShadButton>
   )

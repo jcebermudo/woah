9e96a80dc86967b4f3a4ec81999a3d96daad7a6e
James <jamestech718@gmail.com>
2025-06-15 06:08:02 +0800

    Refactor Card component to enhance structure by integrating Elements for Text and Button, improving layout management with Craft.js.

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -1,21 +1,19 @@
 // components/user/Card.js
-import React  from "react";
+import {useNode, Element} from "@craftjs/core";
+import { Container } from "./Container";
 import { Text } from "./Text";
 import { Button } from "./Button";
-import { Container } from "./Container";
-import { useNode } from "@craftjs/core";
 
-export const Card = ({background, padding = 20}) => {
-  const { connectors: {connect, drag} } = useNode();
+export const Card = ({background, padding}) => {
   return (
-    <Container ref={ref => connect(drag(ref))} background={background} padding={padding}>
-      <div className="text-only">
+    <Container background={background} padding={padding}>
+      <Element id="Text" canvas is={Container}>
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
-      </div>
-      <div className="buttons-only">
-        <Button size="small" text="Learn more" variant="default" color="primary" />
-      </div>
+      </Element>
+      <Element id="Button" canvas is={Container}>
+        <Button size="small" text="Learn more" />
+      </Element>
     </Container>
   )
 }

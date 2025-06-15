e38c920c1803c8fc2c63063f41eb4e947460e731
James <jamestech718@gmail.com>
2025-06-15 15:20:35 +0800

    Update App component to include CardTop and CardBottom subcomponents in the Editor resolver, enhancing layout capabilities and integrating a Button for improved interactivity.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Topbar } from "./components/Topbar";
 
 import { Container } from "./components/user/Container";
 import { Button } from "./components/user/Button";
-import { Card } from "./components/user/Card";
+import { Card, CardTop, CardBottom } from "./components/user/Card";
 import { Text } from "./components/user/Text";
 
 export default function App() {
@@ -18,12 +18,13 @@ export default function App() {
       <h1 className="text-xl font-semibold text-center">
         A super simple page editor
       </h1>
-      <Editor resolver={{ Card, Button, Text, Container }}>
+      <Editor resolver={{ Card, Button, Text, Container, CardTop, CardBottom }}>
         <div className="flex gap-3">
           <div className="flex-1">
             <Frame>
               <Element is={Container} padding={5} background="#eee" canvas>
                 <Card background="#fff" padding={5} />
+            
                 <Button size="sm" variant="outline" color="default">
                   Click
                 </Button>

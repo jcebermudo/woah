a4dc76b2fcd12d1e95ea4ba1578e093c9165cf12
James <jamestech718@gmail.com>
2025-06-15 20:45:09 +0800

    Enhance Text component functionality by integrating content editing capabilities using react-contenteditable, allowing for dynamic text updates and improved user interaction.

--- app/components/user/text.jsx
+++ app/components/user/text.jsx
@@ -1,14 +1,38 @@
-// components/user/Text.js
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ContentEditable from "react-contenteditable";
 import { useNode } from "@craftjs/core";
 
 export const Text = ({ text, fontSize }) => {
   const {
     connectors: { connect, drag },
-  } = useNode();
+    hasSelectedNode,
+    hasDraggedNode,
+    actions: { setProp },
+  } = useNode((state) => ({
+    hasSelectedNode: state.events.selected,
+    hasDraggedNode: state.events.dragged,
+  }));
+
+  const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    !hasSelectedNode && setEditable(false);
+  }, [hasSelectedNode]);
+
   return (
-    <div ref={(ref) => connect(drag(ref))}>
-      <p style={{ fontSize }}>{text}</p>
+    <div ref={(ref) => connect(drag(ref))} onClick={(e) => setEditable(true)}>
+      <ContentEditable
+        disabled={!editable}
+        html={text}
+        onChange={(e) =>
+          setProp(
+            (props) =>
+              (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, "")),
+          )
+        }
+        tagName="p"
+        style={{ fontSize: `${fontSize}px` }}
+      />
     </div>
   );
 };

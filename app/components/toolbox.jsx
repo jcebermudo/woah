97aa3b6c8c1fdbbd5514efebfce8182cb4ae986c
James <jamestech718@gmail.com>
2025-06-15 17:06:21 +0800

    Refactor Toolbox component to enhance functionality by integrating drag-and-drop capabilities for Button, Text, Container, and Card elements, while improving code readability through consistent formatting.

--- app/components/toolbox.jsx
+++ app/components/toolbox.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/app/components/user/Button";
+import { useEditor } from "@craftjs/core";
+import { Text } from "@/app/components/user/Text";
+import { Container } from "@/app/components/user/Container";
+import { Card } from "@/app/components/user/Card";
+
 
 export const Toolbox = () => {
+  const { connectors, query } = useEditor();
+
   return (
     <div className="px-2 py-2">
       <div className="flex flex-col items-center justify-center gap-1">
@@ -9,16 +16,45 @@ export const Toolbox = () => {
           <p className="text-sm">Drag to add</p>
         </div>
         <div className="flex flex-col w-full">
-          <Button variant="default">Button</Button>
+          <Button
+            ref={(ref) =>
+              connectors.create(ref, <Button text="Click me" size="small" />)
+            }
+            variant="default"
+          >
+            Button
+          </Button>
         </div>
         <div className="flex flex-col w-full">
-          <Button variant="default">Text</Button>
+          <Button
+            ref={(ref) => connectors.create(ref, <Text text="Hi world" />)}
+            variant="default"
+          >
+            Text
+          </Button>
         </div>
         <div className="flex flex-col w-full">
-          <Button variant="default">Container</Button>
+          <Button
+            ref={(ref) =>
+              connectors.create(
+                ref,
+                <Element is={Container} padding={20} canvas />,
+              )
+            }
+            variant="default"
+          >
+            Container
+          </Button>
         </div>
         <div className="flex flex-col w-full">
-          <Button variant="default">Card</Button>
+          <div className="flex flex-col w-full">
+            <Button
+              ref={(ref) => connectors.create(ref, <Card />)}
+              variant="default"
+            >
+              Card
+            </Button>
+          </div>
         </div>
       </div>
     </div>

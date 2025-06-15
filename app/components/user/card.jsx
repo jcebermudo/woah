6898177c7713a1d5055780d6dd4600a9121de7ff
James <jamestech718@gmail.com>
2025-06-15 15:20:30 +0800

    Refactor Card component to introduce CardTop and CardBottom subcomponents, enhancing drag-and-drop functionality with specific rules for Text and Button elements in Craft.js.

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -1,17 +1,53 @@
 // components/user/Card.js
-import {useNode, Element} from "@craftjs/core";
-import { Container } from "./Container";
-import { Text } from "./Text";
-import { Button } from "./Button";
+import React  from "react";
+import Text from "./Text";
+import Button from "./Button";
+import { Element, useNode } from "@craftjs/core";
 
-export const Card = ({background, padding}) => {
+import { Container }  from "./Container";
+
+// Notice how CardTop and CardBottom do not specify the drag connector. This is because we won't be using these components as draggables; adding the drag handler would be pointless.
+
+export const CardTop = ({children}) => {
+  const { connectors: {connect} } = useNode();
+  return (
+    <div ref={connect} className="text-only">
+      {children}
+    </div>
+  )
+}
+
+CardTop.craft = {
+  rules: {
+    // Only accept Text
+    canMoveIn: (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Text)
+  }
+}
+
+export const CardBottom = ({children}) => {
+  const { connectors: {connect} } = useNode();
+  return (
+    <div ref={connect}>
+      {children}
+    </div>
+  )
+}
+
+CardBottom.craft = {
+  rules: {
+    // Only accept Buttons
+    canMoveIn : (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Button)
+  }
+}
+
+export const Card = ({background, padding = 20}) => {
   return (
     <Container background={background} padding={padding}>
-      <Element id="Text" canvas is={Container}>
+      <Element id="text" is={CardTop} canvas>
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
       </Element>
-      <Element id="Button" canvas is={Container}>
+      <Element id="buttons" is={CardBottom} canvas>
         <Button size="small" text="Learn more" />
       </Element>
     </Container>

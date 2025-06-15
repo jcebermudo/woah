6377028c796c184dbd0fc71674b7efa45709bd7d
James <jamestech718@gmail.com>
2025-06-15 21:58:15 +0800

    Enhance SettingsPanel component to display selected node details and conditionally render settings and delete button functionality, improving user interaction and component usability.

--- app/components/SettingsPanel.jsx
+++ app/components/SettingsPanel.jsx
@@ -2,31 +2,56 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
 import { Badge } from "@/components/ui/badge";
+import { useEditor } from "@craftjs/core";
 
 export const SettingsPanel = () => {
-  return (
+  const { actions, selected } = useEditor((state) => {
+    const [currentNodeId] = state.events.selected;
+    let selected;
+
+    if (currentNodeId) {
+      selected = {
+        id: currentNodeId,
+        name: state.nodes[currentNodeId].data.name,
+        settings:
+          state.nodes[currentNodeId].related &&
+          state.nodes[currentNodeId].related.settings,
+        isDeletable: query.node(currentNodeId).isDeletable()
+      };
+    }
+
+    return {
+      selected,
+    };
+  });
+
+  return selected ? (
     <div className="bg-muted/60 mt-2 px-2 py-2">
       <div className="flex flex-col gap-4">
         <div className="pb-2">
           <div className="flex items-center justify-between">
             <span className="text-sm font-medium">Selected</span>
-            <Badge variant="default">Selected</Badge>
+            <Badge variant="default">{selected.name}</Badge>
           </div>
         </div>
 
-        <div className="space-y-2">
-          <label className="text-sm font-medium">Prop</label>
-          <Slider
-            defaultValue={[0]}
-            max={50}
-            min={7}
-            step={1}
-            className="w-full"
-          />
-        </div>
+        {selected.settings && React.createElement(selected.settings)}
 
         <Button variant="default">Delete</Button>
+        {
+          selected.isDeletable ? (
+            <MaterialButton
+              variant="contained"
+              color="default"
+              onClick={() => {
+                actions.delete(selected.id);
+              }}
+            >
+              Delete
+            </MaterialButton>
+          ) : null
+        }
       </div>
     </div>
-  );
+  ) : null;
 };

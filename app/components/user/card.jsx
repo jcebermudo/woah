9435773d233dc0ca09e704c89a32a3ccb3a0921a
James <jamestech718@gmail.com>
2025-06-15 17:06:39 +0800

    Refactor Card component for improved code readability by standardizing formatting, including consistent use of spacing and line breaks in props.

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -1,55 +1,61 @@
 // components/user/Card.js
-import React  from "react";
+import React from "react";
 import { Text } from "./Text";
 import { Button } from "./Button";
 import { Element, useNode } from "@craftjs/core";
 
-import { Container }  from "./Container";
+import { Container } from "./Container";
 
 // Notice how CardTop and CardBottom do not specify the drag connector. This is because we won't be using these components as draggables; adding the drag handler would be pointless.
 
-export const CardTop = ({children}) => {
-  const { connectors: {connect} } = useNode();
+export const CardTop = ({ children }) => {
+  const {
+    connectors: { connect },
+  } = useNode();
   return (
     <div ref={connect} className="text-only">
       {children}
     </div>
-  )
-}
+  );
+};
 
 CardTop.craft = {
   rules: {
     // Only accept Text
-    canMoveIn: (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Text)
-  }
-}
+    canMoveIn: (incomingNodes) =>
+      incomingNodes.every((incomingNode) => incomingNode.data.type === Text),
+  },
+};
 
-export const CardBottom = ({children}) => {
-  const { connectors: {connect} } = useNode();
-  return (
-    <div ref={connect}>
-      {children}
-    </div>
-  )
-}
+export const CardBottom = ({ children }) => {
+  const {
+    connectors: { connect },
+  } = useNode();
+  return <div ref={connect}>{children}</div>;
+};
 
 CardBottom.craft = {
   rules: {
     // Only accept Buttons
-    canMoveIn : (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Button)
-  }
-}
+    canMoveIn: (incomingNodes) =>
+      incomingNodes.every((incomingNode) => incomingNode.data.type === Button),
+  },
+};
 
-export const Card = ({background, padding = 20}) => {
+export const Card = ({ background, padding = 20 }) => {
   return (
     <Container background={background} padding={padding}>
-      <Element id="text" is={CardTop} canvas> // Canvas Node of type CardTop
+      <Element id="text" is={CardTop} canvas>
+        {" "}
+        // Canvas Node of type CardTop
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
       </Element>
-      <Element id="buttons" is={CardBottom} canvas> // Canvas Node of type CardBottom
+      <Element id="buttons" is={CardBottom} canvas>
+        {" "}
+        // Canvas Node of type CardBottom
         <Button size="sm" variant="outline" color="default" text="Click" />
       </Element>
     </Container>
-  )
-}
+  );
+};

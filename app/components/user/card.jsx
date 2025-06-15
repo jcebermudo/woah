6ed4fb9ace10fdb677f4e0ee21a47d410747e3fe
James <jamestech718@gmail.com>
2025-06-15 15:39:40 +0800

    working card

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -1,55 +1,64 @@
 // components/user/Card.js
-import React  from "react";
-import Text from "./Text";
-import Button from "./Button";
+import React from "react";
+import { Text } from "./Text";
+import { Button } from "./Button";
 import { Element, useNode } from "@craftjs/core";
-
-import { Container }  from "./Container";
+import { Container } from "./Container";
 
 // Notice how CardTop and CardBottom do not specify the drag connector. This is because we won't be using these components as draggables; adding the drag handler would be pointless.
 
-export const CardTop = ({children}) => {
-  const { connectors: {connect} } = useNode();
+export const CardTop = ({ children }) => {
+  const {
+    connectors: { connect },
+  } = useNode();
   return (
     <div ref={connect} className="text-only">
       {children}
     </div>
-  )
-}
+  );
+};
 
 CardTop.craft = {
+  displayName: "CardTop",
   rules: {
     // Only accept Text
-    canMoveIn: (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Text)
-  }
-}
+    canMoveIn: (incomingNodes) =>
+      incomingNodes.every((incomingNode) => incomingNode.data.type === Text),
+  },
+};
 
-export const CardBottom = ({children}) => {
-  const { connectors: {connect} } = useNode();
-  return (
-    <div ref={connect}>
-      {children}
-    </div>
-  )
-}
+export const CardBottom = ({ children }) => {
+  const {
+    connectors: { connect },
+  } = useNode();
+  return <div ref={connect}>{children}</div>;
+};
 
 CardBottom.craft = {
+  displayName: "CardBottom",
   rules: {
     // Only accept Buttons
-    canMoveIn : (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Button)
-  }
-}
+    canMoveIn: (incomingNodes) =>
+      incomingNodes.every((incomingNode) => incomingNode.data.type === Button),
+  },
+};
 
-export const Card = ({background, padding = 20}) => {
+export const Card = ({ background, padding = 20 }) => {
+
   return (
-    <Container background={background} padding={padding}>
+    <Container
+      background={background}
+      padding={padding}
+    >
       <Element id="text" is={CardTop} canvas>
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
       </Element>
       <Element id="buttons" is={CardBottom} canvas>
-        <Button size="small" text="Learn more" />
+        <Button size="sm" variant="outline" color="default">
+          Click
+        </Button>
       </Element>
     </Container>
-  )
-}
+  );
+};

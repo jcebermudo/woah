f213f531fcc34d6f9e59317b0995ed1ff9361133
James <jamestech718@gmail.com>
2025-06-15 17:15:27 +0800

    Refactor App component to improve code clarity by renaming Button import to UserButton, enhancing consistency in component usage within the Editor resolver.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { SettingsPanel } from "./components/SettingsPanel";
 import { Topbar } from "./components/Topbar";
 
 import { Container } from "./components/user/Container";
-import { Button } from "./components/user/Button";
+import { Button as UserButton } from "./components/user/Button";
 import { Card, CardTop, CardBottom } from "./components/user/Card";
 import { Text } from "./components/user/Text";
 
@@ -18,13 +18,13 @@ export default function App() {
       <h1 className="text-xl font-semibold text-center">
         A super simple page editor
       </h1>
-      <Editor resolver={{ Card, Button, Text, Container, CardTop, CardBottom }}>
+      <Editor resolver={{ Card, UserButton, Text, Container, CardTop, CardBottom }}>
         <div className="flex gap-3">
           <div className="flex-1">
             <Frame>
               <Element is={Container} padding={5} background="#eee" canvas>
                 <Card background="#fff" padding={5} />
-                <Button
+                <UserButton
                   size="sm"
                   variant="outline"
                   color="default"

56e175a2838479e7edb2aa550e0e64a99f48eb3a
James <jamestech718@gmail.com>
2025-06-15 15:51:24 +0800

    Refactor Button component to improve code readability by standardizing formatting and updating the prop name from 'children' to 'text'.

--- app/components/user/button.jsx
+++ app/components/user/button.jsx
@@ -1,14 +1,20 @@
 // components/user/Button.js
-import React  from "react";
+import React from "react";
 import { Button as ShadButton } from "@/components/ui/button";
 import { useNode } from "@craftjs/core";
 
-export const Button = ({size, variant, color, children}) => {
-  const { connectors: {connect, drag} } = useNode();
+export const Button = ({ size, variant, color, text}) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
   return (
-    <ShadButton ref={ref => connect(drag(ref))} size={size} variant={variant} color={color}>
-      {children}
+    <ShadButton
+      ref={(ref) => connect(drag(ref))}
+      size={size}
+      variant={variant}
+      color={color}
+    >
+      {text}
     </ShadButton>
-  )
-}
-
+  );
+};

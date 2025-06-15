a0af8efe0fc6bfc908816e22e9e3e99c3ab4a4c2
James <jamestech718@gmail.com>
2025-06-15 15:51:29 +0800

    Refactor Card component for improved code readability by standardizing formatting, simplifying return statements, and ensuring consistent use of self-closing tags.

--- app/components/user/card.jsx
+++ app/components/user/card.jsx
@@ -1,64 +1,55 @@
 // components/user/Card.js
-import React from "react";
+import React  from "react";
 import { Text } from "./Text";
 import { Button } from "./Button";
 import { Element, useNode } from "@craftjs/core";
-import { Container } from "./Container";
+
+import { Container }  from "./Container";
 
 // Notice how CardTop and CardBottom do not specify the drag connector. This is because we won't be using these components as draggables; adding the drag handler would be pointless.
 
-export const CardTop = ({ children }) => {
-  const {
-    connectors: { connect },
-  } = useNode();
+export const CardTop = ({children}) => {
+  const { connectors: {connect} } = useNode();
   return (
     <div ref={connect} className="text-only">
       {children}
     </div>
-  );
-};
+  )
+}
 
 CardTop.craft = {
-  displayName: "CardTop",
   rules: {
     // Only accept Text
-    canMoveIn: (incomingNodes) =>
-      incomingNodes.every((incomingNode) => incomingNode.data.type === Text),
-  },
-};
+    canMoveIn: (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Text)
+  }
+}
 
-export const CardBottom = ({ children }) => {
-  const {
-    connectors: { connect },
-  } = useNode();
-  return <div ref={connect}>{children}</div>;
-};
+export const CardBottom = ({children}) => {
+  const { connectors: {connect} } = useNode();
+  return (
+    <div ref={connect}>
+      {children}
+    </div>
+  )
+}
 
 CardBottom.craft = {
-  displayName: "CardBottom",
   rules: {
     // Only accept Buttons
-    canMoveIn: (incomingNodes) =>
-      incomingNodes.every((incomingNode) => incomingNode.data.type === Button),
-  },
-};
+    canMoveIn : (incomingNodes) => incomingNodes.every(incomingNode => incomingNode.data.type === Button)
+  }
+}
 
-export const Card = ({ background, padding = 20 }) => {
-
+export const Card = ({background, padding = 20}) => {
   return (
-    <Container
-      background={background}
-      padding={padding}
-    >
-      <Element id="text" is={CardTop} canvas>
+    <Container background={background} padding={padding}>
+      <Element id="text" is={CardTop} canvas> // Canvas Node of type CardTop
         <Text text="Title" fontSize={20} />
         <Text text="Subtitle" fontSize={15} />
       </Element>
-      <Element id="buttons" is={CardBottom} canvas>
-        <Button size="sm" variant="outline" color="default">
-          Click
-        </Button>
+      <Element id="buttons" is={CardBottom} canvas> // Canvas Node of type CardBottom
+        <Button size="sm" variant="outline" color="default" text="Click" />
       </Element>
     </Container>
-  );
-};
+  )
+}

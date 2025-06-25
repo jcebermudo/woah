60a89c861508785ca8700e1cfb438cd62d852c4d
James <jamestech718@gmail.com>
2025-06-26 01:29:17 +0800

    Add SVG icon to toolbar and adjust layout with gap for improved spacing

--- app/page.tsx
+++ app/page.tsx
@@ -755,7 +755,19 @@ const App: React.FC = () => {
     <div className="bg-white min-h-screen relative">
       {/* Toolbar */}
       <div className="absolute top-0 left-0 w-full bg-white border-b-1 border-[#E3E3E3] z-10 p-[15px]">
-        <div className="flex flex-row items-center justify-between h-full w-full">
+        <div className="flex flex-row items-center justify-between h-full w-full gap-[10px]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="31"
+            height="26"
+            fill="none"
+            viewBox="0 0 31 26"
+          >
+            <path
+              fill="#000"
+              d="M14.647 17.733c-1.155 3.816-1.563 8.138-6.457 8.07-2.175-.033-3.636-.1-4.791-2.16-1.7-3.04-4.69-19.283-2.787-22.018C1.326.61 4.214.476 5.404.443c.815-.034 1.733-.034 2.345.54C9.04 2.199 8.904 11.519 9.278 13.917c.068.54.306 1.79 1.02 1.823 2.446.102.985-10.03 2.242-11.988.748-1.216 2.957-1.148 4.146-.743.476.169.986.405 1.292.81 1.257 1.622.034 8.477.951 11.179.136.371.408 1.013.884.979.408-.034.748-.71.85-1.047 1.121-3.107.475-12.292 2.413-14.217.543-.54 1.257-.743 2.005-.71 1.189.068 4.282.473 5.063 1.351.476.54.816 1.284.85 2.027.17 3.31-3.195 16.952-4.826 19.992-.51.945-1.257 1.857-2.31 2.16-1.326.372-2.822.609-4.181.372-3.67-.641-4.35-5.133-5.03-8.172"
+            />
+          </svg>
           <div className="flex items-center justify-between h-full w-full">
             <span className="text-[16px] font-semibold text-black">
               Untitled

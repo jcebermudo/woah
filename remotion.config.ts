1aae4d1b4454bd9ce090a5f4f450bf8642f93828
James <jamestech718@gmail.com>
2025-06-25 19:39:38 +0800

    Remove Remotion configuration file as part of the cleanup process following the removal of Remotion packages.

--- remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-Config.setConcurrency(2);
-Config.setCodec('h264');
-Config.setImageSequence(false); 

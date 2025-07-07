76906c9fcf04b181608cbcd69d9bcc6dbd5a39b7
James <jamestech718@gmail.com>
2025-07-07 17:54:37 +0800

    sortable item props

--- app/components/dnd/SortableItem.tsx
+++ app/components/dnd/SortableItem.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-export default function SortableItem({id, children}: {id: string, children: React.ReactNode}) {
+export default function SortableItem({
+  id,
+  children,
+}: {
+  id: string;
+  children: React.ReactNode;
+}) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: id });
 

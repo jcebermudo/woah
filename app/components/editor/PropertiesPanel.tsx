84c67c44dd08e90f3d137f66a93a5898e83ce273
James <jamestech718@gmail.com>
2025-07-07 19:35:44 +0800

    Properties Panel

---
+++ app/components/editor/PropertiesPanel.tsx
@@ -0,0 +1,7 @@
+export default function PropertiesPanel() {
+    return (
+      <div className="h-screen pt-[80px] px-[10px] overflow-y-auto">
+        <h1>Properties Panel</h1>
+      </div>
+    );
+}

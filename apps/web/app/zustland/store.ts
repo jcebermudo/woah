1127b349771ad8df3ec94d3e450914b927b33a30
James <jamestech718@gmail.com>
2025-07-20 03:10:44 +0800

    duration zustand

--- apps/web/app/zustland/store.ts
+++ apps/web/app/zustland/store.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 type Store = {
   mode: "design" | "animate";
   setMode: (mode: "design" | "animate") => void;
+  duration: number;
+  setDuration: (duration: number) => void;
 };
 
 export const useStore = create<Store>((set) => ({
   mode: "design",
   setMode: (mode: "design" | "animate") => set({ mode }),
+  duration: 100,
+  setDuration: (duration: number) => set({ duration }),
 }));
